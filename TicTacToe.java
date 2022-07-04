5d44bc6acba167c6218527fe9c76f91433fd10b8
Ulvi <102907551+oolyvi@users.noreply.github.com>
2022-07-04 18:22:31 +0400

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,167 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToe {
+	
+	//static deyer
+	static ArrayList<Integer> playerPositions = new ArrayList<>();
+	static ArrayList<Integer> cpuPositions = new ArrayList<>();
+	
+	//print	method
+	public static void printGameBoard(char[][] gameBoard) {
+		for (char[] row : gameBoard) {
+			for (char c : row) {
+				System.out.print(c);
+			}
+			System.out.println();
+		}
+	}
+	//yerlesdirme
+	public static void placePiece(char[][] gameBoard, int pos, String user) {
+		
+		char symbol = ' ';
+		
+		if (user.equals("player")) {
+			symbol = 'X';
+			playerPositions.add(pos);  //elave edib o yeri rezerv edir kimi
+		} else if (user.equals("cpu")) {
+			symbol = 'O';
+			cpuPositions.add(pos);      //elave edib o yeri rezerv edir kimi
+		}
+		
+		switch (pos) {
+		case 1:
+			gameBoard[0][0] = symbol;
+			break;
+		case 2:
+			gameBoard[0][2] = symbol;
+			break;
+		case 3:
+			gameBoard[0][4] = symbol;
+			break;
+		case 4:
+			gameBoard[2][0] = symbol;
+			break;
+		case 5:
+			gameBoard[2][2] = symbol;
+			break;
+		case 6:
+			gameBoard[2][4] = symbol;
+			break;
+		case 7:
+			gameBoard[4][0] = symbol;
+			break;
+		case 8:
+			gameBoard[4][2] = symbol;
+			break;
+		case 9:
+			gameBoard[4][4] = symbol;
+			break;
+		default:
+			break;	
+	}
+}
+	//qazanani gosteren
+	public static String checkWinner() {
+		
+		List topRow = Arrays.asList(1, 2, 3);
+		List midRow = Arrays.asList(4, 5, 6);
+		List botRow = Arrays.asList(7, 8, 9);
+		List leftCol = Arrays.asList(1, 4, 7);
+		List midCol = Arrays.asList(2, 5, 8);
+		List rightCol = Arrays.asList(3, 6, 9);
+		List cross1 = Arrays.asList(1, 5, 9);
+		List cross2 = Arrays.asList(3, 5, 7);
+		//yuxaridaki winning condileri bir liste add etdik
+		List<List> winning = new ArrayList<List>();
+		winning.add(topRow);
+		winning.add(midRow);
+		winning.add(botRow);
+		winning.add(leftCol);
+		winning.add(midCol);
+		winning.add(rightCol);
+		winning.add(cross1);
+		winning.add(cross2);
+		
+		for (List l : winning) {
+			if (playerPositions.containsAll(l)) 
+				return "Congrulations, You Won!";
+				
+			 else if (cpuPositions.containsAll(l)) 
+				return "Sorry, CPU won :(";
+			 else if (cpuPositions.size() + playerPositions.size() == 9) //bu yerler bitiyini ve hec hece
+				 return "Cat )";           
+			
+		}
+		return "";
+	}
+	
+	
+
+	
+	//main////////////////////////////////////////
+	public static void main(String[] args) {
+		
+		char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+							{'-', '+', '-', '+', '-'},
+							{' ', '|', ' ', '|', ' '},
+							{'-', '+', '-', '+', '-'},
+							{' ', '|', ' ', '|', ' '},};
+			
+		printGameBoard(gameBoard);
+				
+		
+		while (true) {
+			Scanner scan = new Scanner(System.in);
+			System.out.print("Enter your placement (1-9): ");
+			int playerPos = scan.nextInt();
+			//bir birinin ustune ve yerine dusmesin deye
+			while(playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)) {
+				System.out.println("Positon Taken! Enter a Correct Position");
+				playerPos = scan.nextInt();
+			}
+			
+			placePiece(gameBoard, playerPos, "player");    //X O nu qoyan	
+			
+			String result = checkWinner();  //2 defe yazdiq
+			if (result.length() > 0) {
+				System.out.println(result);
+				break;
+			}
+			
+			
+			
+			Random rand = new Random();
+			int cpuPos = rand.nextInt(9) + 1;        //1 den 9 a
+			//bir birinin ustune ve yerine dusmesin deye
+			while(playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)) {
+				cpuPos = rand.nextInt(9) + 1;
+			}
+			
+			placePiece(gameBoard, cpuPos, "cpu");		 
+			
+			
+			printGameBoard(gameBoard);
+			
+			result = checkWinner();     //2 defe yazdiq hem yuxarida hem asagida
+			if (result.length() > 0) {
+				System.out.println(result);
+				break;
+			}
+
+			
+		}
+		
+			
+		
+		
+		
+
+	}
+
+}
